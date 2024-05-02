bb9747dde760a4254c85258976b7d4cbbd7888e2
tom <tom@sysprog.myguest.virtualbox.org>
2024-05-02 12:03:51 -0500

    Assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,4 +1,3 @@
-// needed for daemon() function and sigaction stuff
 #define _DEFAULT_SOURCE
 #define __USE_XOPEN
 
@@ -17,40 +16,221 @@
 #include <fcntl.h>
 #include <syslog.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include "queue.h"
 
 #define PORT "9000"
 #define BACKLOG 10
 #define TEMPFILE "/var/tmp/aesdsocketdata"
-#define MAX_LINE_LENGTH 32768
+#define MAX_LINE_LENGTH 65536
 
-int server_fd;
-int connection_fd;
-int temp_fd;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void sigterm_handler(int signo) {
-    syslog(LOG_DEBUG, "Caught signal, exiting");
+static int server_fd;
+static int temp_fd;
+static pthread_t timer_thread;
+static int running_as_usual;
+
+struct node {
+	pthread_t thread;
+	SLIST_ENTRY(node) next;
+};
+
+SLIST_HEAD(head_s, node) head;
+
+struct timer_thread_arg_s {
+    int num_secs;
+    int exit_code;
+};
+
+struct client_thread_arg_s {
+	int connection_fd;
+    int exit_code;
+};
+
+void *timer_thread_worker(void *thread_param) {
+    struct timespec tp0, tp1;
+    char timestr[25];
+    size_t b_written;
+    time_t rawtime;
+    int interval = 10;
+
+    clock_gettime(CLOCK_MONOTONIC, &tp0);
+
+    while (running_as_usual) {
+        do {
+            clock_gettime(CLOCK_MONOTONIC, &tp1);
+            usleep(50000);
+        } while (tp1.tv_sec - tp0.tv_sec < interval && running_as_usual);
+
+        if (running_as_usual) {
+            // get a head start on the next time delta (pointless!)
+            clock_gettime(CLOCK_MONOTONIC, &tp0);
+
+            // timestamp:00001122334455"
+            time(&rawtime);
+            strftime(timestr, 26, "timestamp:%Y%m%d%H%M%S\n", localtime(&rawtime));
+
+            pthread_mutex_lock(&mutex);
+
+            b_written = write(temp_fd, timestr, strlen(timestr));
+
+            pthread_mutex_unlock(&mutex);
+
+            if (b_written == -1) {
+                perror("write");
+                syslog(LOG_ERR, "Error writing to temp file.");
+            }
+        }
+    }
+
+    return thread_param;
+}
+
+void *client_thread_worker(void *thread_param) {
+    struct client_thread_arg_s* thread_args = (struct client_thread_arg_s*) thread_param;
+
+	int conn_fd = thread_args->connection_fd;
+
+	// read from client until newlinez
+	char curr;
+	char line[MAX_LINE_LENGTH + 2];
+
+	memset(&line, '\0', MAX_LINE_LENGTH + 2);
+
+	int index = 0;
+	int ret;
+
+    do {
+        ret = recv(conn_fd, &curr, 1, 0);
+
+		if (ret == 1) {
+            if (curr != '\r' && curr != '\n') {
+                line[index++] = curr;
+
+                if (index > MAX_LINE_LENGTH) {
+                    // empty the string so we ignore this oversized line
+                    line[0] = '\0';
+                    break;
+                }
+            }
+        } else if (ret == -1 && (errno != EAGAIN || errno != EWOULDBLOCK)) {
+            thread_args->exit_code = 1;
+        }
+	} while (curr != '\n' && !thread_args->exit_code);
+
+	if (!thread_args->exit_code && strlen(line) > 0) {
+		// printf("Bytes: %lu; Len: %lu; Heard: '%s'\n", strlen(line) * sizeof(char), strlen(line), line);
+		line[index] = '\n';
+
+		pthread_mutex_lock(&mutex);
+		
+		// we are writing in append mode
+		ret = write(temp_fd, line, strlen(line));
+
+		pthread_mutex_unlock(&mutex);
+
+		if (ret == -1) {
+			perror("write");
+            thread_args->exit_code = 1;
+		}
+	}
+
+    if (!thread_args->exit_code) {
+        int offset = 0;
+
+        // get one line at a time from TEMPFILE
+        do {
+            index = 0;
+
+            memset(&line, '\0', MAX_LINE_LENGTH + 1);
+
+            pthread_mutex_lock(&mutex);
+
+            lseek(temp_fd, offset, SEEK_SET);
+            
+            while (read(temp_fd, &curr, sizeof(char) && index <= MAX_LINE_LENGTH) > 0) {
+                line[index++] = curr;
+
+                if (curr == '\n') {
+                    line[index] = '\0';
+
+                    break;
+                }
+            }
+
+            pthread_mutex_unlock(&mutex);
+
+            if (index > MAX_LINE_LENGTH) {
+                line[0] = '\0';
+            }
+
+            offset += index;
+
+            if (strlen(line) > 0) {
+                while ((ret = send(conn_fd, line, strlen(line) * sizeof(char), MSG_NOSIGNAL)) == -1 && (errno == EAGAIN || errno == EWOULDBLOCK));
+
+                if (ret == -1) {
+                    thread_args->exit_code = 1;
+                }
+            }
+        } while (!thread_args->exit_code && strlen(line) > 0);
+    }
+
+    close(conn_fd);
+
+	return thread_param;
+}
+
+void cleanup() {
+    running_as_usual = false;
+
+    pthread_mutex_lock(&mutex);
 
     shutdown(server_fd, SHUT_RD);
-
     sleep(1);
-
     shutdown(server_fd, SHUT_WR);
+    
+    pthread_mutex_unlock(&mutex);
+
+    void* thread_rtn;
+
+	// loop over the threads and join them one by one
+	while (!SLIST_EMPTY(&head)) {
+        struct node *first = SLIST_FIRST(&head);
+
+		if ((pthread_join(first->thread, &thread_rtn)) != 0) {
+			syslog(LOG_WARNING, "Could not client thread.");
+		}
+
+        free(thread_rtn);
+
+		SLIST_REMOVE_HEAD(&head, next);
+
+		free(first);
+		first = NULL;
+	}
+
+    if (pthread_join(timer_thread, &thread_rtn) != 0) {
+        syslog(LOG_WARNING, "Could not join timer thread.");
+    }
+
+    free(thread_rtn);
 
     close(server_fd);
     close(temp_fd);
     
     remove(TEMPFILE);
 
-    exit(0);
+    closelog();
 }
 
+void sigterm_handler(int signo) {
+    syslog(LOG_DEBUG, "Caught signal, exiting");
 
-void sigchld_handler(int signo) {
-    int saved_errno = errno;
-    
-    while(waitpid(-1, NULL, WNOHANG) > 0);
-
-    errno = saved_errno;
+	cleanup();
+	
+    exit(EXIT_SUCCESS);
 }
 
 void *get_in_addr(struct sockaddr *sa) {
@@ -62,17 +242,19 @@ void *get_in_addr(struct sockaddr *sa) {
 }
 
 int main (int argc, char **argv) {
-    openlog(NULL, 0, LOG_USER);
+    running_as_usual = true;
 
+    openlog(NULL, 0, LOG_USER);
+    
     if (argc > 1) {
         if ((strcmp(argv[1], "-d")) == 0) {
             if (daemon(0, 0) != 0) {
                 syslog(LOG_ERR, "Could not daemonize.");
-                exit(1);
+                exit(EXIT_FAILURE);
             }
         } else {
             syslog(LOG_ERR, "Invalid arguments provided.");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
     }
 
@@ -82,7 +264,7 @@ int main (int argc, char **argv) {
 
     struct sockaddr_storage remote_addr;
     socklen_t sin_size;
-    struct sigaction sa, sa2;
+    struct sigaction sa;
 
     int yes = 1;
     
@@ -96,31 +278,27 @@ int main (int argc, char **argv) {
 
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        syslog(LOG_ERR, "getaddrinfo:%s", gai_strerror(rv));
 
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     // loop and bind
     for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((server_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("aesdsocket: socket");
-            syslog(LOG_WARNING, "socket");
 
             continue;
         }
 
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
             perror("setsockopt");
-            syslog(LOG_ERR, "setsockopt");
 
-            exit(1);
+            exit(EXIT_FAILURE);
         }
 
         if (bind(server_fd, p->ai_addr, p->ai_addrlen) == -1) {
             close(server_fd);
             perror("aesdsocket: bind");
-            syslog(LOG_WARNING, "bind");
 
             continue;
         }
@@ -132,52 +310,49 @@ int main (int argc, char **argv) {
 
     if (p == NULL) {
         fprintf(stderr, "aesdsocket: failed to bind\n");
-        syslog(LOG_ERR, "failed to bind");
 
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, BACKLOG) == -1) {
         perror("listen");
-        syslog(LOG_ERR, "listen");
 
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    sa.sa_handler = sigchld_handler;
+    sa.sa_handler = sigterm_handler;
 
     sigemptyset(&sa.sa_mask);
-    
+
     sa.sa_flags = SA_RESTART;
-    
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        syslog(LOG_ERR, "sigaction (sigchld_handler)");
 
-        exit(1);
+    if (sigaction(SIGTERM, &sa, NULL) == -1 || sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction");
+
+        exit(EXIT_FAILURE);
     }
 
-    sa2.sa_handler = sigterm_handler;
+	// ready the tempfile
+    temp_fd = open(TEMPFILE, O_CREAT | O_RDWR | O_APPEND, 0400 | 0200);
 
-    sigemptyset(&sa2.sa_mask);
+	if (temp_fd < 0) {
+		fprintf(stderr, "Could not open temp file");
+		
+        exit(EXIT_FAILURE);
+	}
 
-    sa2.sa_flags = SA_RESTART;
+    // init the client thread list
+	SLIST_INIT(&head);
 
-    if (sigaction(SIGTERM, &sa2, NULL) == -1 || sigaction(SIGINT, &sa2, NULL) == -1) {
-        perror("sigaction");
-        syslog(LOG_ERR, "sigaction (sigterm_handler)");
-
-        exit(1);
-    }
-
-    // printf("aesdsocket: waiting for connections...\n");
+    // start the timer thread
+    pthread_create(&timer_thread, NULL, timer_thread_worker, NULL);
 
     while (true) {
         sin_size = sizeof(remote_addr);
 
-        connection_fd = accept(server_fd, (struct sockaddr *)&remote_addr, &sin_size);
+        int connection_fd = accept(server_fd, (struct sockaddr *)&remote_addr, &sin_size);
 
-        if (connection_fd == -1) {
+        if (connection_fd == -1 && !running_as_usual) {
             perror("accept");
             syslog(LOG_WARNING, "accept");
 
@@ -190,121 +365,35 @@ int main (int argc, char **argv) {
             s, sizeof(s)
         );
 
-        // printf("aesdsocket: got connection from %s\n", s);
         syslog(LOG_DEBUG, "Accepted connection from %s", s);
 
-        if (!fork()) {
-            close(server_fd);
+        struct client_thread_arg_s *thread_args = (struct client_thread_arg_s*) malloc(sizeof(struct client_thread_arg_s));
 
-            temp_fd = open(TEMPFILE, O_CREAT | O_RDWR | O_APPEND, 0400 | 0200);
+        thread_args->connection_fd = connection_fd;
+        thread_args->exit_code = 0;
 
-            if (temp_fd < 0) {
-                syslog(LOG_WARNING, "could not open temp file");
+		pthread_t thread_id;
 
-                // just send an empty string back
-                if (send(connection_fd, "\n", 1, 0) == -1) {
-                    syslog(LOG_ERR, "Error sending to %s", s);
-                    close(connection_fd);
-                    
-                    return 1;
-                }
-            }
+        if (pthread_create(&thread_id, NULL, client_thread_worker, thread_args) != 0) {
+			syslog(LOG_ERR, "Could not create thread!");
+			cleanup();
 
-            // read from client until newline
-            char curr;
+			exit(EXIT_FAILURE);
+		}
+		
+        struct node *elem = malloc(sizeof(struct node));
 
-            char line[MAX_LINE_LENGTH + 2];
-            memset(&line, '\0', MAX_LINE_LENGTH + 2);
-
-            int index = 0;
-
-            while (true) {
-                recv(connection_fd, &curr, 1, 0);
-
-                if (curr == '\n') {
-                    break;
-                } else if (curr != '\r') {
-                    line[index++] = curr;
-
-                    //if (strlen(line) > MAX_LINE_LENGTH) {
-                    if (index > MAX_LINE_LENGTH) {
-                        // printf("line is '%s'; len is %lu.\n", line, strlen(line));
-
-                        syslog(LOG_ERR, "Max line length exceded (in).");
-                        
-                        // empty the string so we ignore this line
-                        line[0] = '\0';
-
-                        break;
-                    }
-                }
-            }
-
-            if (strlen(line) > 0) {
-                // printf("Bytes: %lu; Len: %lu; Heard: '%s'\n", strlen(line) * sizeof(char), strlen(line), line);
-                line[index] = '\n';
-
-                // we are writing in append mode
-                size_t b_written = write(temp_fd, line, strlen(line));
-
-                if (b_written == -1) {
-                    perror("write");
-                    syslog(LOG_ERR, "Error writing to temp file.");
-
-                    exit(1);
-                }
-            }
-
-            // we need to reset the pointer to beginning of file
-            lseek(temp_fd, 0, SEEK_SET);
-
-            // get one line at a time from TEMPFILE
-            while (true) {
-                index = 0;
-
-                memset(&line, '\0', MAX_LINE_LENGTH + 1);
-
-                while (read(temp_fd, &curr, sizeof(char)) > 0) {
-                    line[index++] = curr;
-
-                    if (index > MAX_LINE_LENGTH) {
-                        syslog(LOG_ERR, "Max line length exceeded (out).");
-
-                        close(connection_fd);
-
-                        exit(1);
-                    }
-
-                    if (curr == '\n') {
-                        line[index] = '\0';
-
-                        break;
-                    }
-                }
-
-                if (index > 0) {
-                    if (send(connection_fd, line, strlen(line) * sizeof(char), 0) == -1) {
-                        syslog(LOG_ERR, "Error sending to %s", s);
-
-                        close(connection_fd);
-
-                        exit(1);
-                    }
-                } else {
-                    // done sending file contents
-                    close(connection_fd);
-
-                    syslog(LOG_DEBUG, "Closed connection from %s", s);
-
-                    exit(0);
-                }
-            }
+        if (elem == NULL) {
+            syslog(LOG_ERR, "malloc failed!");
+            exit(EXIT_FAILURE);
         }
 
-        close(connection_fd);
-    }
+        elem->thread = thread_id;
 
-    return 0;
+		SLIST_INSERT_HEAD(&head, elem, next);
+    }
+ 
+    return EXIT_SUCCESS;
 }
 
 // use valgrind to check for memory leaks
