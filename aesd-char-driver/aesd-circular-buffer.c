67b976cd564c9eeb0b5c1a75db166e5b7ca36fbe
tom <tom@sysprog.myguest.virtualbox.org>
2024-05-05 22:06:57 -0500

    Assignment 7 part 1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -15,6 +15,8 @@
 #endif
 
 #include "aesd-circular-buffer.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
@@ -32,7 +34,38 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
-    return NULL;
+    struct aesd_buffer_entry *retval = NULL;
+
+    uint8_t index;
+    
+    printf("\n\npassed %lu\n", char_offset);
+
+    struct aesd_buffer_entry *curr = &(buffer->entry[buffer->out_offs]);
+
+    if (curr) {
+        AESD_CIRCULAR_BUFFER_FOREACH(curr, buffer, index) {
+            printf("size %lu\n", curr->size);
+
+            if (char_offset > curr->size - 1) {
+                char_offset = char_offset - curr->size;
+                printf("char_offset %lu\n", char_offset);
+            } else {
+                // we're here. populate the ptr w/ char and return this entry
+                printf("found %s\n", curr->buffptr);
+                retval = curr;
+
+                size_t as_size_t = (size_t)(curr->buffptr[char_offset]);
+                printf("char there is '%c'\n", curr->buffptr[char_offset]);
+                printf("ascii val is %lu\n", as_size_t);
+
+                entry_offset_byte_rtn = &as_size_t;
+
+                break;
+            }
+        }
+    }
+
+    return retval;
 }
 
 /**
@@ -47,6 +80,19 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    printf("\nadding '%s'\n", add_entry->buffptr);
+    printf("in_offs %i\n", buffer->in_offs);
+    printf("out_offs %i\n", buffer->out_offs);
+
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    uint8_t new_wpos = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    buffer->in_offs = new_wpos;
+
+    if (new_wpos == buffer->out_offs) {
+        uint8_t new_rpos = (new_wpos + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        buffer->out_offs = new_rpos;
+    }
 }
 
 /**
